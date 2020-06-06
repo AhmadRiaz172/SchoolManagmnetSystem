a7a79b70e51cb8c25069d886132de347a7fa7f7a
Ahmad Riaz <ahmadriaz1999@gmail.com>
2020-06-06 20:47:50 +0500

    special insertion query updated

--- sql/sample_query_for_run_time_data_insertion.sql
+++ sql/sample_query_for_run_time_data_insertion.sql
@@ -1,2 +1,2 @@
 insert into parent(parentid, mothername, fathername,edate)
-select max(parentid) + 1 , 'Khujista','Shabih' , to_date( '04-may-20','dd-mon-yy' ) from parent ;  
+select COALESCE(max(parentid),0)+1 , 'Khujista','Shabih' , to_date( '04-may-20','dd-mon-yy' ) from parent ;  
